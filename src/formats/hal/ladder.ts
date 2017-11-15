1f4b0d7d28b5b167d434534187190ca46e6198ab
Evert Pot <evert@yelp.com>
2017-11-14 23:27:56 -0800

    Some resource titles

--- src/formats/hal/ladder.ts
+++ src/formats/hal/ladder.ts
@@ -18,11 +18,15 @@ export function model(ladder: Ladder) {
   return {
     _links: {
       self: { href: '/ladders/' + ladder.key },
-      game: { href: '/games/' + ladder.game.key },
+      game: {
+        href: '/games/' + ladder.game.key,
+        title: ladder.game.title
+      },
       ranking: { href: '/ladders/' + ladder.key + '/rankings' },
       allowedStages: ladder.allowedStages.map( stage => {
         return {
-          href: '/games/' + stage.game.key + '/stages/' + stage.key
+          href: '/games/' + stage.game.key + '/stages/' + stage.key,
+          title: stage.name
         };
       })
     },
