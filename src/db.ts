ef7bd88afb5e72817d0580a1c14f192556b12b62
Evert Pot <evert@yelp.com>
2017-11-16 11:36:09 -0800

    Using environment variables for database

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,9 @@
 const mysql2 = require('mysql2/promise');
 const pool = mysql2.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'smash_ladder'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DB,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 export default pool;
