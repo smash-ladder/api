4b8ea6b6f81d46e40d48f7ac4ebe21de63b6c637
Evert Pot <evert@yelp.com>
2017-11-21 18:44:36 -0800

    limit 4

--- src/services/ladder.ts
+++ src/services/ladder.ts
@@ -22,7 +22,7 @@ export class LadderService {
         allowedStages: [dreamLand],
         allowedItems: 'All items, except Hammer, Maxim Tomato, Heart Container',
         lives: 5,
-        challengeRankingLimit: 3,
+        challengeRankingLimit: 4,
         algorithm: 'bump'
       };
       return ladder;
